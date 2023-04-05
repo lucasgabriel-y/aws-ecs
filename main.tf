9fc223b00e972e91fd52e80d31fa4576ff68d875
lucasgabriel-y <gabriel.lglg93@gmail.com>
2023-04-05 10:36:04 -0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -9,24 +9,42 @@ resource "aws_ecs_cluster" "my_cluster" {
 
 resource "aws_ecs_task_definition" "my_task_definition" {
   family                = "myapp"
-  requires_compatibilities = ["FARGATE"]
+  #requires_compatibilities = ["FARGATE"]
   network_mode = "awsvpc"
-  container_definitions = file("/container.json")
-
+  cpu       = 1024
+  memory    = 2048
+  container_definitions = jsonencode([
+    {
+        name      = "wordpress"
+        image     = "wordpress"
+        cpu       = 1024
+        memory    = 2048
+        essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
 }
+
+  
+
 
 resource "aws_ecs_service" "my_service" {
   name            = "my-service"
   cluster         = aws_ecs_cluster.my_cluster.id
   task_definition = aws_ecs_task_definition.my_task_definition.arn
   desired_count   = 1
-  #launch_type     = "FARGATE"
-
+  launch_type     = "FARGATE"
+#https://earthly.dev/blog/deploy-dockcontainers-to-awsecs-using-terraform/
 
   network_configuration {
     security_groups  = [aws_security_group.public_security_group.id]
     subnets          = [aws_subnet.public_subnet.id]
-    #assign_public_ip = true
+    assign_public_ip = true
   }
 }
 
